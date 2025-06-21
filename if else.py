81ad0b2de3c09a9b530182bcb8bd36ee18773b9f
lanjeron15 <howjwbb@gmail.com>
2025-06-21 19:27:57 +0200

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,38 @@
+#health = 80
+
+#if health > 50:
+  #  print("Ты в порядке")
+#else:
+ #   print("Тебе срочно нужен хил!")
+
+#if health > 80:
+#    print("Ты герой!")
+#elif health > 50:
+#    print("Ты в порядке")
+#else:
+#    print("Ты почти мёртв...")
+
+#health = int(input("Сколько у тебя здоровья? "))
+#weapon = input("У тебя есть меч? (да/нет): ")
+
+#if health > 70 and weapon == "да":
+#    print("Ты готов к битве! ")
+#else:
+#    print("Нужно потренироваться перед боем...")
+
+#print("=== Игра: Ты готов к битве? ===")
+
+#name = input("Имя героя: ")
+#health = int(input("Здоровье (0–100): "))
+#weapon = input("Есть ли у тебя оружие? (да/нет): ")
+#magic = input("Ты умеешь магию? (да/нет): ")
+
+#print("\n Проверка готовности...")
+
+#if health >= 70 and weapon == "да":
+#    print(f"{name} — готов к битве! ⚔")
+#elif magic == "да" and health >= 50:
+#    print(f"{name} не очень силён, но магия спасёт! ")
+#else:
+#    print(f"{name}, тебе ещё рано идти в бой. Попробуй позже. ")
+
